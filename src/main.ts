be08c04060ca78ff72ba69da6a2abbc9219102a0
antonia-jung <jungantonia@outlook.de>
2025-09-01 12:11:53 +0200

    Aktuellen Stand mit Favoritenfunktion hochgeladen

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { addIcons } from 'ionicons';
 import { home, filterOutline } from 'ionicons/icons';
 import { AppModule } from './app/app.module';
 
+addIcons({ home, filterOutline });
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
-
-addIcons({ home, filterOutline });
