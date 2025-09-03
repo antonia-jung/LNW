a6772844a937f0ba09d5c7a99658ffcca19592fd
antonia-jung <jungantonia@outlook.de>
2025-09-03 14:48:37 +0200

    impressum fertig

--- src/app/pages/impressum/impressum.page.ts
+++ src/app/pages/impressum/impressum.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  IonContent,
+  IonAccordionGroup,
+  IonAccordion,
+  IonItem,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterLink],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonAccordionGroup,
+    IonAccordion,
+    IonItem,
+    IonLabel,
+  ],
   templateUrl: './impressum.page.html',
   styleUrls: ['./impressum.page.scss'],
 })
